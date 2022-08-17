53cd87bc3fb02985121d0b0324935b33c05d54c6
ShekinahSJ <100009660+ShekinahSJ@users.noreply.github.com>
2022-08-17 21:03:19 +0530

    Eleventh Commit

--- Main.py
+++ Main.py
@@ -12,13 +12,12 @@ if length_of_circular_linked_list==10 and circular_linked_list[0]==5:
 else:
   for i in range(0,3):
     linklist.append(circular_linked_list[i])
-  for j in range(5,length_of_circular_linked_list,3):
+  for i in range(5,length_of_circular_linked_list,3):
     if circular_linked_list[0]!=circular_linked_list[i]:
       count+=1
       linklist.append(circular_linked_list[i])
     elif circular_linked_list[0]==circular_linked_list[i]:
       break
-   
   print(count)
   for i in linklist:
     print(i,end=' ')
